e3f21649fbff8f4e08b157d067206b26af598e52
TOJI <151569591+HelpMeToPush@users.noreply.github.com>
2026-02-05 01:05:32 +0200

    odd or even class
    Fix syntax errors in even/odd check and improve comments.

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    // tyring to check if this number is even or odd TO TEST GIT_PUSH
+    int x  = 10;
+
+    if ( x % 2 == 0 ) {
+      System.out.println("EVEN");
+  } else if ( x == 0 ) {
+      System.out.Println("zero:")
+    } else {
+        // tyring to avoid 0 inputs x % 2 != 0
+      System.out.Println("ODD:")
+  
+        }
+  
+}
