d722a6fd2f313c0d9c48d07c098c29a8ca2028af
TOJI <151569591+HelpMeToPush@users.noreply.github.com>
2026-02-05 01:08:39 +0200

    TESTING CLASS

---
+++ test_2.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    // just pring any thing to test commit 
+    System.out.println("TESTING SOMETHING HERE FOR GIT___PUSH_OR____PULLL");
+  }  
+}
